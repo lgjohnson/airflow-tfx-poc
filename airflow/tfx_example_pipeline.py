4bd437aa9830111595a993d23f0495a3365737f5
lgjohnson <gregleejohnson@gmail.com>
2019-06-21 12:53:42 -0700

    directory constants in  pipeline

--- airflow/tfx_example_pipeline.py
+++ airflow/tfx_example_pipeline.py
@@ -14,15 +14,24 @@ from tfx.components.statistics_gen.component import StatisticsGen
 from tfx.components.trainer.component import Trainer
 from tfx.components.transform.component import Transform
 from tfx.orchestration.tfx_runner import TfxRunner
+from tfx.utils.dsl_utils import csv_input
 
 # Airflow config parameters
 PIPELINE_NAME = os.environ['PIPELINE_NAME']
 SCHEDULE_INTERVAL = os.environ.get('SCHEDULE_INTERVAL', None)
 
 # Directory variables
-HOME_DIR = os.path.join(os.environ['HOME'], 'airflow/')
-BASE_DIR = os.path.join(HOME_DIR, 'data', PIPELINE_NAME)
-OUTPUT_DIR = os.path.join(BASE_DIR, 'pipelines/')
+HOME_DIR = os.environ['AIRFLOW_HOME']       # airflow home directory
+DAGS_DIR = os.environ['DAGS_DIR']           # directory for pipeline code
+DATA_DIR = os.environ['DATA_DIR']           # directory for input data
+METADATA_DIR = os.environ['METADATA_DIR']   # directory for metadata
+PLUGINS_DIR = os.environ['PLUGINS_DIR']     # directory for plugins
+SERVING_DIR = os.environ['SERVING_DIR']     # directory to output models
+
+# Model Code Files
+TRANSFORM_MODULE_FILE = os.environ['TRANSFORM_MODULE_FILE']  # transform code
+MODEL_MODULE_FILE = os.environ['MODEL_MODULE_FILE']          # model train code
+
 
 @PipelineDecorator(
     pipeline_name=PIPELINE_NAME,
@@ -36,12 +45,12 @@ OUTPUT_DIR = os.path.join(BASE_DIR, 'pipelines/')
         )
     },
     metadata_db_root=os.path.join(HOME_DIR, 'data/tfx/metadata'),
-    pipeline_root=pipeline_root
+    pipeline_root=DAGS_DIR
 )
 def create_pipeline():
 
     # Read data in; can split data here
-    examples = csv_input(os.path.join(BASE_DIR, 'no_split/span_1'))
+    examples = csv_input(DATA_DIR)
     example_gen = CsvExampleGen(input_data=examples)
 
     # Generate feature statistics
@@ -56,16 +65,16 @@ def create_pipeline():
         schema=infer_schema.outputs.output
     )
 
-    # Performs feature engineering; emits a SavedModel that performs preprocessing
+    # Performs feature engineering; emits a SavedModel that does preprocessing
     transform = Transform(
         input_data=example_gen.outputs.output,
         schema=infer_schema.outputs.output,
-        module_file=transforms
+        module_file=TRANSFORM_MODULE_FILE
     )
 
     # Trains a model
     trainer = Trainer(
-        module_file=model,
+        module_file=MODEL_MODULE_FILE,
         transformed_examples=transform.outputs.transformed_examples,
         schema=infer_schema.outputs.output,
         transform_output=transform.outputs.transform_output,
@@ -86,11 +95,11 @@ def create_pipeline():
         model=trainer.outputs.output
     )
 
-    # Pushes a blessed model to a deployment target  (e.g. a TensorFlow Serving instance)
+    # Pushes a blessed model to a deployment target (tfserving)
     pusher = Pusher(
         model_export=trainer.outputs.output,
         model_blessing=model_validator.outputs.blessing,
-        serving_model_dir=serving_model_dir
+        serving_model_dir=SERVING_DIR
     )
 
     return [
